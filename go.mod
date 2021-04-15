27e0d137b005c46676ce5b90ac4d1bf62bbc53ae
hmarf <hmarfUni@gmail.com>
2021-04-15 12:23:11 +0900

    最低限のoptionを追加

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/fatih/color v1.10.0
 	github.com/stretchr/testify v1.7.0
+	github.com/urfave/cli v1.22.5
 	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
 )
--- main.go
+++ main.go
@@ -8,18 +8,64 @@ import (
 	"strings"
 
 	"github.com/fatih/color"
+	"github.com/urfave/cli"
 	"golang.org/x/term"
 )
 
-func main() {
-	if err := cTest(); err != nil {
+type Option struct {
+	Run  bool
+	Pass bool
+	Fail bool
+}
+
+func App() *cli.App {
+	app := cli.NewApp()
+	app.Name = "cTest"
+	app.Usage = "Give color to the output according to the test result."
+	app.Version = "0.0.1"
+	app.Author = "hmarf"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "run, r",
+			Usage: "Do not output '=== RUN: ~'",
+		},
+		cli.BoolFlag{
+			Name:  "pass, p",
+			Usage: "Do not output '--- PASS: ~'",
+		},
+		cli.BoolFlag{
+			Name:  "fail, f",
+			Usage: "Do not output '--- FAIL: ~'",
+		},
+	}
+	return app
+}
+
+func Action(c *cli.Context) {
+	app := App()
+	if c.String("input") == "None" {
+		app.Run(os.Args)
+		return
+	}
+	option := Option{
+		Run:  c.Bool("run"),
+		Pass: c.Bool("pass"),
+		Fail: c.Bool("fail"),
+	}
+	if err := cTest(option); err != nil {
 		fmt.Println(err)
 	}
 }
 
-func cTest() error {
+func main() {
+	app := App()
+	app.Action = Action
+	app.Run(os.Args)
+}
+
+func cTest(o Option) error {
 	if !term.IsTerminal(int(os.Stdin.Fd())) {
-		if err := readLines(os.Stdin); err != nil {
+		if err := readLines(os.Stdin, o); err != nil {
 			return err
 		}
 		return nil
@@ -28,10 +74,10 @@ func cTest() error {
 }
 
 // reference: https://www.yunabe.jp/tips/golang_readlines.html
-func readLines(f *os.File) error {
+func readLines(f *os.File, o Option) error {
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		colorString(s.Text())
+		colorString(s.Text(), o)
 	}
 	if s.Err() != nil {
 		return s.Err()
@@ -40,24 +86,30 @@ func readLines(f *os.File) error {
 }
 
 // if you want to change text color, modify this function
-func colorString(line string) {
+func colorString(line string, o Option) {
 	trimmed := strings.TrimSpace(line)
 	switch {
 	case strings.HasPrefix(trimmed, "=== RUN"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "?"):
-		fmt.Println(line)
+		if !o.Run {
+			fmt.Println(line)
+		}
 
 	case strings.HasPrefix(trimmed, "ok"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "PASS"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "--- PASS"):
-		color.Green(line)
+		if !o.Pass {
+			color.Green(line)
+		}
 
 	case strings.HasPrefix(trimmed, "--- FAIL"):
 		fallthrough
 	case strings.HasPrefix(trimmed, "FAIL"):
-		color.Red(line)
+		if !o.Fail {
+			color.Red(line)
+		}
 	}
 }
